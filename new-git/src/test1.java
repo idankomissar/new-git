a1e063fd48c40d21c6155c224c48e9af3a106424
user <user@10.0.0.21>
2020-10-22 13:24:06 +0300

    third commit

--- new-git/src/test1.java
+++ new-git/src/test1.java
@@ -4,6 +4,7 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("my name is Idan");
+		System.out.println("this is the last commit of the day");
 
 	}
 
